279313558c9dc883a1af5f62dff20f8f2bada408
Federico Nahuel Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2025-04-27 15:11:07 +0000

    Update: Creamos estructuras administrativas de memoria

--- memoria/src/include/memoria-state.h
+++ memoria/src/include/memoria-state.h
@@ -3,6 +3,8 @@
 
 // Incluimos las bibliotecas necesarias:
 #include <stdlib.h>
+#include <utils/logger.h>
+#include <commons/collections/dictionary.h>
 
 // Incluimos bibliotecas propias:
 #include "memoria-types.h"
@@ -10,5 +12,10 @@
 // Definimos las variables globales:
 extern t_config_memoria *config_memoria;
 extern t_memoria_servers *memoria_servers;
+extern t_dictionary *instrucciones_por_procesos;
+extern t_dictionary *metricas_por_procesos;
+extern void *espacio_usuario;
+
+void memoria_state_init();
 
 #endif // CPU_STATE_H_
