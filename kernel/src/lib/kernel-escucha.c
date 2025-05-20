6665f5c72f3bf667006f671be762a1063ec46a9f
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2025-05-20 22:31:12 +0000

    Update: Agrego procedimiento syscall DUMP MEMORY

--- kernel/src/lib/kernel-escucha.c
+++ kernel/src/lib/kernel-escucha.c
@@ -72,29 +72,31 @@ void kernel_server_dispatch_handler(int cpu_socket, int operation, const char *s
     case DUMP_MEMORY:
         t_pcb* process = recibir_proceso(new_buffer);
         
-        queue_process(process, BLOCKED);
+        log_info(logger,"Se recibio la syscall DUMP MEMORY desde el server %s",server_name);
 
         set_cpu(cpu_socket, DISPONIBLE);
 
-        //avisar_dump_memory(process->pid);
+        queue_process(process, BLOCKED);
 
-
-        // como se acaba de liberar una CPU, enviamos el primer proceso de READY a EXECUTE 
-        //enviar_proceso_cpu(cpu_socket, list_get(planner->short_term->queue_READY->queue_ESTADO, 0)); 
-
-        //ENTIENDO la logica, pero creo que la funcion de enviar a cpu solo deberia llamarse en un unico lugar,
-        // que es la queue de execute. SI QUE DEBERIA "ACTIVARSE UNA SECUENCIA PARA QUE SUCEDA"
-        
-
-        log_info(logger,"Se recibio la syscall INIC_PROC desde el server %s",server_name);
-        free(process);
+        if(avisar_dump_memory(process->pid) == 0)
+        {
+            queue_process(process, READY);
+        }
+        else
+        {
+            queue_process(process, EXIT);
+        }
+    
+        //free(process);???
         break;
 
     case IO:
+        log_info(logger,"Se recibio la syscall IO desde el server %s",server_name);
+
         process = recibir_proceso(new_buffer);
         queue_process(process, BLOCKED);
 
-
+        set_cpu(cpu_socket, DISPONIBLE);
         // como se acaba de liberar una CPU, enviamos el primer proceso de READY a EXECUTE 
         //enviar_proceso_cpu(cpu_socket, list_get(planner->short_term->queue_READY->queue_ESTADO, 0));
          
@@ -102,8 +104,8 @@ void kernel_server_dispatch_handler(int cpu_socket, int operation, const char *s
         // que es la queue de execute. SI QUE DEBERIA "ACTIVARSE UNA SECUENCIA PARA QUE SUCEDA"
 
 
-        log_info(logger,"Se recibio la syscall IO desde el server %s",server_name);
-        free(process);
+        
+        //free(process); ???
         break;
 
     case EXIT_Sys:
