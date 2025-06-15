3de7dc945972642e54451eed2fccf2f79b1d0873
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2025-06-15 03:28:10 +0000

    Cambio a list_remove en trar_proceso_a_MP

--- kernel/src/lib/kernel-planner.c
+++ kernel/src/lib/kernel-planner.c
@@ -159,15 +159,15 @@ void traer_proceso_a_MP(){
     //   
     //    if(solicitar_a_memoria(desuspender_proceso, list_get(planner->long_term->queue_NEW->cola,0)))
     //    {
-    //        queue_process(list_get(planner->medium_term->queue_READY_SUSPENDED->cola,0), READY);
+    //        queue_process(list_remove(planner->medium_term->queue_READY_SUSPENDED->cola,0), READY);
     //    } else { break; }
     //}
 
     while(!list_is_empty(planner->long_term->queue_NEW->cola)){ 
 
-        if(!solicitar_a_memoria(memoria_init_proc, list_get(planner->long_term->queue_NEW->cola,0)))
+        if(solicitar_a_memoria(memoria_init_proc, list_get(planner->long_term->queue_NEW->cola,0)))
         {
-            queue_process(list_get(planner->long_term->queue_NEW->cola,0), READY);
+            queue_process(list_remove(planner->long_term->queue_NEW->cola,0), READY);
         } else { break; }
     }
 }
