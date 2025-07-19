8bd66a1152db97837d084f746a0e318cbd4ad121
Federico Nahuel Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2025-07-19 12:02:34 -0300

    Update swap-funtions.c

--- memoria/src/lib/swap-funtions.c
+++ memoria/src/lib/swap-funtions.c
@@ -104,10 +104,10 @@ void swap_in(char* pid_key, int pid, int client_socket) {
         
         // Liberamos la memoria dinámica de las estructuras y variables asociadas utilizadas en la función: OK
         list_destroy(marcos_libres);
+        usleep(config_memoria->RETARDO_SWAP * 1000); // Simulamos retardo de escritura
     }
 
     free(pid_key);
-    usleep(config_memoria->RETARDO_SWAP * 1000); // Simulamos retardo de escritura
     log_info(logger, "Proceso PID %d: swap-in finalizado con estado %s", pid, status);
     send(client_socket, &resquest, sizeof(resquest), 0);
 } 
